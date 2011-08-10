be15463aa561508678667fa2b762fc72a0e1a69d
Evan Borgstrom <evan@fatbox.ca>
2011-08-10 15:22:38 -0400

    DRY

--- longerusername/forms.py
+++ longerusername/forms.py
@@ -4,20 +4,21 @@ from django import forms
 
 from longerusername import MAX_USERNAME_LENGTH
 
+def update_username_field(field):
+    field.max_length = MAX_USERNAME_LENGTH()
+    field.help_text = _("Required. Only letters, numbers, and @, ., +, -, or _ characters.")
+
 class UserCreationForm(auth_forms.UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].max_length = MAX_USERNAME_LENGTH()
-        self.fields['username'].help_text = _("Required. Only letters, numbers, and @, ., +, -, or _ characters.")
+        update_username_field(self.fields['username'])
 
 class UserChangeForm(auth_forms.UserChangeForm):
     def __init__(self, *args, **kwargs):
         super(UserChangeForm, self).__init__(*args, **kwargs)
-        self.fields['username'].max_length = MAX_USERNAME_LENGTH()
-        self.fields['username'].help_text = _("Required. Only letters, numbers, and @, ., +, -, or _ characters.")
+        update_username_field(self.fields['username'])
 
 class AuthenticationForm(auth_forms.AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(AuthenticationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].max_length = MAX_USERNAME_LENGTH()
-        self.fields['username'].help_text = _("Required. Only letters, numbers, and @, ., +, -, or _ characters.")
+        update_username_field(self.fields['username'])
